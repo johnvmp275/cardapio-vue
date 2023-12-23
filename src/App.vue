56076f84e8dde308572ec94a434b33fe9073a7c0
John.pm <marquesjoaov1@gmail.com>
2023-12-23 17:23:19 -0300

    s

--- src/App.vue
+++ src/App.vue
@@ -99,8 +99,8 @@ a.router-link-active span {
 .indicador::before {
   content: '';
   position: absolute;
-  top: -28.2px;
-  right: -7.4px;
+  top: -27px;
+  right: -6px;
   width: 20px;
   height: 20px;
   border-bottom-right-radius: 10px;
