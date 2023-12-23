7d5a212b26eff24df375b394798c9362742065ee
John.pm <marquesjoaov1@gmail.com>
2023-12-23 16:22:58 -0300

    animacao ao mudar de menu

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,19 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <div class="router-link">
-          <RouterLink to="/">Menu</RouterLink>
-          <RouterLink to="/pedidos">Pedidos</RouterLink>
+          <RouterLink to="/">
+            <span class="material-symbols-outlined">
+              store
+            </span>
+          </RouterLink>
+          <RouterLink to="/pedidos">
+            <span class="material-symbols-outlined">
+              shopping_cart
+            </span>
+          </RouterLink>
+          <div class="indicador">
+            <div class="indicador-list"></div>
+          </div>
         </div>
       </nav>
     </div>
@@ -18,12 +29,97 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped>
 nav {
-  padding: 16px;
-  width: 100%;
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  padding: 17px;
+  align-items: center;
+  /* justify-content: space-between; */
+  flex-direction: column;
+  background: #1F1D2B;
+  border-radius: 0 8px 8px 0;
 }
 
 .router-link {
+  margin-top: 40px;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   gap: 18px;
+}
+
+a {
+  position: relative;
+  display: flex;
+  align-items: center;
+  position: relative;
+  justify-content: center;
+  width: 75px;
+  height: 75px;
+  cursor: pointer;
+  z-index: 10;
+  transition-delay: 125ms;
+}
+
+a span {
+  z-index: 10;
+  transition: .5s;
+  color: #EA7C69;
+  font-size: 34px;
+}
+
+.indicador-list {
+  width: 75px;
+  height: 75px;
+  background: #EA7C69;
+  box-shadow: 0 0 20px 0px #EA7C69;
+  border-radius: 10px;
+}
+
+a.router-link-active span {
+  color: #fff;
+}
+
+.indicador {
+  position: fixed;
+  width: 100px;
+  height: 90px;
+  left: 10px;
+  top: 50px;
+  background: #fff;
+  border-radius: 8px 0 0 8px;
+  border: 8px solid white;
+  transition: .5s;
+}
+
+.indicador::before {
+  content: '';
+  position: absolute;
+  top: -27.2px;
+  right: -6.5px;
+  width: 20px;
+  height: 20px;
+  border-bottom-right-radius: 10px;
+  background: #1F1D2B;
+  box-shadow: 0 10px 0 0 white;
+}
+
+.indicador::after {
+  content: '';
+  position: absolute;
+  right: -6.5px;
+  bottom: -28px;
+  width: 20px;
+  height: 20px;
+  border-top-right-radius: 10px;
+  background: #1F1D2B;
+  box-shadow: 0 -10px 0 0 white;
+}
+
+
+.router-link a.router-link-active:nth-child(2).router-link-active~.indicador {
+  transform: translateY(calc(93px * 1));
 }
 </style>
