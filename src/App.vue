82cdcdd886f7dfa0b0e31c3cc5df687e7104fb4e
John.pm <marquesjoaov1@gmail.com>
2024-01-04 22:15:40 -0300

    t

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import { RouterLink } from 'vue-router'
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <Navbar />
+   <nav>
+    <section class="navigation-section">
+      <div class="navigation-link">
+        <RouterLink to="/">
+          <span class="material-symbols-outlined">
+            store
+          </span>
+        </RouterLink>
+        <RouterLink to="/pedidos">
+          <span class="material-symbols-outlined">
+            shopping_cart
+          </span>
+        </RouterLink>
+        <RouterLink to="/config" class="animation-rotate">
+          <span class="material-symbols-outlined">
+            settings
+          </span>
+        </RouterLink>
+        <div class="indicador">
+          <div class="indicador-list"></div>
+        </div>
+      </div>
+    </section>
+  </nav>
   <RouterView />
   <Footer />
 </template>
