90c115e3e43b86d1c50f6c93f27f9542df7cd329
John.pm <marquesjoaov1@gmail.com>
2024-01-04 22:17:46 -0300

    edwed

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
-import { RouterLink } from 'vue-router'
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
-   <nav>
-    <section class="navigation-section">
-      <div class="navigation-link">
-        <RouterLink to="/">
-          <span class="material-symbols-outlined">
-            store
-          </span>
-        </RouterLink>
-        <RouterLink to="/pedidos">
-          <span class="material-symbols-outlined">
-            shopping_cart
-          </span>
-        </RouterLink>
-        <RouterLink to="/config" class="animation-rotate">
-          <span class="material-symbols-outlined">
-            settings
-          </span>
-        </RouterLink>
-        <div class="indicador">
-          <div class="indicador-list"></div>
-        </div>
-      </div>
-    </section>
-  </nav>
+  <Navbar />
   <RouterView />
   <Footer />
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,35 @@
 <script>
-
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
- 
+  <nav>
+    <section class="navigation-section">
+      <div class="navigation-link">
+        <RouterLink to="/">
+          <span class="material-symbols-outlined">
+            store
+          </span>
+        </RouterLink>
+        <RouterLink to="/pedidos">
+          <span class="material-symbols-outlined">
+            shopping_cart
+          </span>
+        </RouterLink>
+        <RouterLink to="/config" class="animation-rotate">
+          <span class="material-symbols-outlined">
+            settings
+          </span>
+        </RouterLink>
+        <div class="indicador">
+          <div class="indicador-list"></div>
+        </div>
+      </div>
+    </section>
+  </nav>
 </template>
 
-<style>
+<style scoped>
 nav {
   width: 107px;
   height: 100vh;
